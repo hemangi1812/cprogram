d66a7ef9755081f434b40927ebe8311b438a5913
hemangi1812 <ranahemangi899@gmail.com>
2026-07-18 12:49:03 +0530

    Add files via upload

---
+++ basics/program1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main ()
+{
+    printf ( "hello world\n");
+    int a = 10;
+    printf("\nvalue of a is %d",a);
+    float b =10.5;
+    printf("\n value of b is %f",b);
+    double d= 1111.11;
+    printf (" \n value of d is %lf", d);
+    char c= 'A';
+    printf(" \n value of c is %c",c );
+
+    
+
+}
+
+
---
+++ basics/user_defined.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(){
+ 
+int i;
+float  f;
+char ch;
+double d;
+printf("\n enter int value");
+scanf("%d",&i);
+printf("int value is %d",i);
+
+printf("\n enter flaot value");
+scanf("%f",&f);1
+printf("float value is %f",f);
+
+printf("\n enter char value");
+scanf("%c", &ch);
+printf("char value is %c", ch);
+
+printf("\n enter double value");
+scanf("%lf",&d);
+printf("double value is %lf",d);
+
+}
